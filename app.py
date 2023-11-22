95fa514f6c19651b6d38d97434b316e5d8fafe03
Ana Monsalve <91292255+amonsalv@users.noreply.github.com>
2023-11-22 17:12:31 +0000

    Add rock-paper-scissors game to app.py

--- app.py
+++ app.py
@@ -9,3 +9,25 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return app.send_static_file("index.html")
+
+import random
+
+opciones = ["piedra", "papel", "tijeras"]
+
+while True:
+    eleccion_jugador = input("Elige una opción: piedra, papel o tijeras: ").lower()
+    if eleccion_jugador not in opciones:
+        print("Opción no válida. Inténtalo de nuevo.")
+        continue
+    eleccion_oponente = random.choice(opciones)
+    if eleccion_jugador == eleccion_oponente:
+        print("Es un empate.")
+    elif (eleccion_jugador == "piedra" and eleccion_oponente == "tijeras") or \
+         (eleccion_jugador == "tijeras" and eleccion_oponente == "papel") or \
+         (eleccion_jugador == "papel" and eleccion_oponente == "piedra"):
+        print("Has ganado.")
+    else:
+        print("Has perdido.")
+    jugar_de_nuevo = input("¿Quieres jugar de nuevo? (s/n): ").lower()
+    if jugar_de_nuevo != "s":
+        break
